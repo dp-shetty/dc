0625809e873a78193ea401502b6a6f42c4ddcdf7
dp-shetty <dps2k811@gmail.com>
2024-10-09 00:46:52 +0530

    updates

--- api/index.js
+++ api/index.js
@@ -9,7 +9,12 @@ const userEmailRoutes = require("../routes/emailUsersRoutes");
 const emailVerifyRoute = require("../routes/emailVerifyRoutes");
 
 // Get frontend URLs from environment variables
-const frontendUrl = process.env.FRONTEND_URL; // "https://dchalios.vercel.app"
+const frontendUrl = process.env.FRONTEND_URL; // e.g., "https://dchalios.vercel.app"
+const frontendLogin = process.env.FRONTEND_LOGIN;
+const frontendLoginAuth = process.env.FRONTEND_LOGIN_AUTH;
+const frontendSignup = process.env.FRONTEND_SIGNUP;
+const frontendSignupAuth = process.env.FRONTEND_SIGNUP_AUTH;
+const frontendAi = process.env.FRONTEND_AI_URL;
 
 // Initialize the app
 const app = express();
@@ -17,18 +22,23 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-// CORS middleware
+// CORS middleware configuration
+const allowedOrigins = [
+  frontendUrl,
+  frontendLogin,
+  frontendLoginAuth,
+  frontendSignup,
+  frontendSignupAuth,
+  frontendAi,
+];
+
+// Configure CORS
 app.use(
   cors({
-    origin: [
-      frontendUrl, 
-      process.env.FRONTEND_LOGIN, 
-      process.env.FRONTEND_LOGIN_AUTH,
-      process.env.FRONTEND_SIGNUP,
-      process.env.FRONTEND_SIGNUP_AUTH,
-      process.env.FRONTEND_AI_URL,
-    ], // Allow multiple frontend URLs
+    origin: allowedOrigins, // Allow multiple frontend URLs
     credentials: true, // Allow credentials (cookies) to be sent
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
   })
 );
 
