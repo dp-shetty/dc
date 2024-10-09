46e8eea91167ac86ee388b23c515fde80fbcdc84
dp-shetty <dps2k811@gmail.com>
2024-10-10 02:50:33 +0530

    added new cookie

--- controllers/jwtVerifyController.js
+++ controllers/jwtVerifyController.js
@@ -1,10 +1,20 @@
+const jwt = require("jsonwebtoken"); // Import JWT library
+
 const getSession = (req, res) => {
   const token = req.cookies.authToken;
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-  res.json({ JWT_Token: req.cookies.authToken });
+
+  // Verify token
+  jwt.verify(token, process.env.JWT_SECC, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    res.json({ JWT_Token: token });
+  });
 };
+
 
 module.exports = {
   getSession,
