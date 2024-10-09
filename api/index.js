3f2aeb705ba00f1e7f9fb48075eea33f329d9e87
dp-shetty <dps2k811@gmail.com>
2024-10-09 18:46:11 +0530

    updated code

--- api/index.js
+++ api/index.js
@@ -15,7 +15,20 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors())
+// Configure CORS to allow requests from specific origins
+const allowedOrigins = ['https://dchalios.vercel.app'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true, // Allow cookies to be sent if necessary
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
