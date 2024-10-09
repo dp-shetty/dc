f1135078e7abad1218872067b394bcaa3d3f3b7a
dp-shetty <dps2k811@gmail.com>
2024-10-09 19:56:28 +0530

    updated nodemailer

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -1,16 +1,18 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  debug: true,
+  logger: true,
 });
 
 const sendMail = (to, subject, html) => {
   const mailOptions = {
-    from:`"D-CHALIOS" <${process.env.EMAIL_USER}>`,
+    from: `"D-CHALIOS" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
@@ -18,9 +20,10 @@ const sendMail = (to, subject, html) => {
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error(error);
+      console.error("Error sending email:", error);
+      return;
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log("Email sent: " + info.response);
     }
   });
 };
