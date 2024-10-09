3266d8e168323fd0e768932c77a35bf50be6cd6e
dp-shetty <dps2k811@gmail.com>
2024-10-10 02:41:54 +0530

    added new cookie

--- controllers/jwtVerifyController.js
+++ controllers/jwtVerifyController.js
@@ -1,35 +1,11 @@
-
-const jwt = require("jsonwebtoken");
-const emailUser = require("../models/emailUsersModel");
-
-const authenticateJWT = (req, res, next) => {
-  const token = req.cookies.authToken; // Get the token from the cookie
+const getSession = (req, res) => {
+  const token = req.cookies.authToken;
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-
-  jwt.verify(token, process.env.JWT_SECC, async (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: "Invalid token" });
-    }
-
-    // Fetch user data if needed
-    const foundUser = await emailUser.findById(user.id); // Assuming the JWT contains the user's ID
-    if (!foundUser) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    req.user = foundUser; // Attach the user to the request object
-    next();
-  });
-};
-
-// Controller for the /session route
-const getSession = (req, res) => {
-  res.json({ JWT_Token: req.cookies.authToken, user: req.user });
+  res.json({ JWT_Token: req.cookies.authToken });
 };
 
 module.exports = {
-  authenticateJWT,
   getSession,
 };
--- routes/jwtVerifyRouter.js
+++ routes/jwtVerifyRouter.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const {
-  authenticateJWT,
-  getSession,
-} = require("../controllers/jwtVerifyController");
+const { getSession } = require("../controllers/jwtVerifyController");
 
 const router = express.Router();
 
 // Session route
-router.get("/session", authenticateJWT, getSession);
+router.get("/session", getSession);
 
 module.exports = router;
