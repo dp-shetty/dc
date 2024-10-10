6db279c25474ed0dbf98c0a48ffdfd4441ef329f
dp-shetty <dps2k811@gmail.com>
2024-10-10 11:46:01 +0530

    added opt

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("../config/db");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const defaultRoute = require("../routes/defaultRoutes");
@@ -26,6 +27,7 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
